529e132b80079b3734e8077b3dac877adc22562a
284569022 <284569022@qq.com>
2023-05-05 17:49:36 +0800

    ok

--- client.go
+++ client.go
@@ -20,16 +20,12 @@ func (c *Client) Database(name string, opts ...*options.DatabaseOptions) *Databa
 }
 
 // WithTransaction open transaction
-func (c *Client) WithTransaction(ctx context.Context, fn func(ctx context.Context) (any, error)) (any, error) {
-	var (
-		res any
-		err error
-	)
-	err = c.client.UseSession(ctx, func(sessionContext mongo.SessionContext) error {
-		res, err = sessionContext.WithTransaction(ctx, func(sessCtx mongo.SessionContext) (any, error) {
-			return fn(sessCtx)
+func (c *Client) WithTransaction(ctx context.Context, fn func(ctx context.Context) error) error {
+	return c.client.UseSession(ctx, func(sessionContext mongo.SessionContext) error {
+		_, err := sessionContext.WithTransaction(ctx, func(sessCtx mongo.SessionContext) (any, error) {
+			return nil, fn(sessCtx)
 		})
+
 		return err
 	})
-	return res, err
 }
