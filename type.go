47f086638ce8a84340ac02c18731119d2bd1bbea
284569022 <284569022@qq.com>
2023-05-12 14:13:04 +0800

    ok

--- type.go
+++ type.go
@@ -1,10 +1,13 @@
 package jmgo
 
 import (
+	"encoding/json"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/bsontype"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/x/bsonx/bsoncore"
+	"strconv"
+	"time"
 )
 
 type MustSObjectId string
@@ -87,4 +90,50 @@ func (th SObjectId) MarshalBSONValue() (bsontype.Type, []byte, error) {
 
 func NewSObjectId() SObjectId {
 	return SObjectId(primitive.NewObjectID().Hex())
+}
+
+type MilliTime time.Time
+
+// UnmarshalBSONValue UnmarshalBSON bson转go对象
+func (th *MilliTime) UnmarshalBSONValue(t bsontype.Type, data []byte) error {
+	milli, _, ok := bsoncore.ReadDateTime(data)
+	if ok {
+		*th = MilliTime(time.UnixMilli(milli))
+	}
+
+	return nil
+}
+
+// MarshalBSONValue go对象转bson
+func (th MilliTime) MarshalBSONValue() (bsontype.Type, []byte, error) {
+	return bson.MarshalValue(time.Time(th))
+}
+
+func (th *MilliTime) UnmarshalJSON(data []byte) error {
+	milli, err := strconv.ParseInt(string(data), 10, 64)
+	if err != nil {
+		return err
+	}
+	*th = MilliTime(time.UnixMilli(milli))
+	return nil
+}
+
+func (th MilliTime) MarshalJSON() ([]byte, error) {
+	return json.Marshal(time.Time(th).UnixMilli())
+}
+
+func Now() MilliTime {
+	return MilliTime(time.Now())
+}
+
+func FromTime(time time.Time) MilliTime {
+	return MilliTime(time)
+}
+
+func NewFromTimePtr(time *time.Time) *MilliTime {
+	return (*MilliTime)(time)
+}
+
+func NewFromTime(time time.Time) *MilliTime {
+	return (*MilliTime)(&time)
 }
